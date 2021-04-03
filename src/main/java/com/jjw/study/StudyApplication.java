f6e65aa6bfa1e87b2068532fb7cb0087ff940ef9
JWJEONG <wldns0202@naver.com>
2021-04-03 16:28:03 +0900

    Initial commit

---
+++ src/main/java/com/jjw/study/StudyApplication.java
@@ -0,0 +1,13 @@
+package com.jjw.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class, args);
+    }
+
+}
